afb065fdae1d5359d492f042884caf2b51892661
AsteroidDown <jasper@stahmer.ca>
2024-11-03 17:05:13 -0500

    Card Print List selected print persistance

--- components/cards/card-prints-list.tsx
+++ components/cards/card-prints-list.tsx
@@ -1,7 +1,7 @@
 import { currency } from "@/functions/text-manipulation";
 import ScryfallService from "@/hooks/scryfall.service";
 import { Card } from "@/models/card/card";
-import { Link } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import React, { useEffect } from "react";
 import { ScrollView, View } from "react-native";
 import Divider from "../ui/divider/divider";
@@ -12,6 +12,8 @@ export interface CardPrintsListProps {
 }
 
 export default function CardPrintsList({ card }: CardPrintsListProps) {
+  const { setId, cardNumber } = useLocalSearchParams();
+
   const [cardPrints, setCardPrints] = React.useState([] as Card[]);
 
   useEffect(() => {
@@ -23,8 +25,8 @@ export default function CardPrintsList({ card }: CardPrintsListProps) {
   }, [card]);
 
   return (
-    <View className="flex gap-2 max-w-[400px]">
-      <View className="flex flex-row">
+    <View className="flex max-w-[400px]">
+      <View className="flex flex-row mb-2">
         <Text thickness="semi">Print</Text>
 
         <View className="flex flex-row gap-2 ml-auto">
@@ -46,7 +48,11 @@ export default function CardPrintsList({ card }: CardPrintsListProps) {
           <Link
             key={print.id + index}
             href={`cards/${print.set}/${print.collectorNumber}`}
-            className="flex flex-row gap-2 p-2 max-w-full border-b border-background-100 hover:bg-primary-300 transition-all duration-300"
+            className={`flex flex-row gap-2 p-2 max-w-full border-b hover:bg-primary-300 transition-all duration-300 ${
+              print.set === setId && print.collectorNumber === cardNumber
+                ? "bg-background-300"
+                : "border-background-100"
+            }`}
           >
             <Text truncate size="sm">
               {print.setName}
@@ -56,6 +62,7 @@ export default function CardPrintsList({ card }: CardPrintsListProps) {
               <Text size="sm" className="w-14">
                 {print.collectorNumber}
               </Text>
+
               <Text size="sm" className="w-14 text-right">
                 {currency(print.prices?.usd)}
               </Text>
