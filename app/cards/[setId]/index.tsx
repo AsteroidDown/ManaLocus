3df8ba76f6d25bbed4921fe1821d493dda75d19d
AsteroidDown <jasper@stahmer.ca>
2024-11-10 11:57:19 -0500

    Added card filtering to set page

--- app/cards/[setId]/index.tsx
+++ app/cards/[setId]/index.tsx
@@ -1,12 +1,21 @@
 import CardList from "@/components/cards/card-list";
 import BoxHeader from "@/components/ui/box/box-header";
+import FilterBar from "@/components/ui/filters/filter-bar";
 import Placeholder from "@/components/ui/placeholder/placeholder";
 import SearchBar from "@/components/ui/search-bar/search-bar";
 import { TabProps } from "@/components/ui/tabs/tab";
 import TabBar from "@/components/ui/tabs/tab-bar";
+import { filterCards } from "@/functions/card-filtering";
+import {
+  sortCardsAlphabetically,
+  sortCardsByCollectorNumber,
+  sortCardsByManaValue,
+  sortCardsByPrice,
+} from "@/functions/card-sorting";
 import ScryfallService from "@/hooks/scryfall.service";
 import { Card } from "@/models/card/card";
 import { Set } from "@/models/card/set";
+import { CardFilters } from "@/models/sorted-cards/sorted-cards";
 import { useLocalSearchParams } from "expo-router";
 import React, { useEffect } from "react";
 import { ScrollView, View } from "react-native";
@@ -19,6 +28,8 @@ export default function SetPage() {
   const [filteredCards, setFilteredCards] = React.useState([] as Card[]);
 
   const [search, setSearch] = React.useState("");
+
+  const [filters, setFilters] = React.useState({} as CardFilters);
 
   const [tabs, setTabs] = React.useState([] as TabProps[]);
 
@@ -44,15 +55,62 @@ export default function SetPage() {
   }, [set]);
 
   useEffect(() => {
-    if (!cards?.length || !search) setFilteredCards(cards);
-    else {
-      setFilteredCards(
-        cards.filter((card) =>
-          card.name.toLowerCase().includes(search.toLowerCase())
-        )
-      );
+    if (!cards?.length) {
+      setFilteredCards(cards);
+      return;
     }
-  }, [search]);
+
+    const searchedCards = cards.filter((card) =>
+      card.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    let sortedCards: Card[] = [];
+
+    if (
+      filters.priceSort ||
+      filters.manaValueSort ||
+      filters.alphabeticalSort
+    ) {
+      if (filters.alphabeticalSort) {
+        sortedCards =
+          filters.alphabeticalSort === "ASC"
+            ? sortCardsAlphabetically(searchedCards)
+            : filters.alphabeticalSort === "DESC"
+            ? sortCardsAlphabetically(searchedCards, false)
+            : searchedCards;
+      }
+
+      if (filters.priceSort) {
+        const cardsToSort: Card[] = sortedCards?.length
+          ? sortedCards
+          : searchedCards;
+
+        sortedCards =
+          filters.priceSort === "ASC"
+            ? sortCardsByPrice(cardsToSort)
+            : filters.priceSort === "DESC"
+            ? sortCardsByPrice(cardsToSort, false)
+            : cardsToSort;
+      }
+
+      if (filters.manaValueSort) {
+        const cardsToSort: Card[] = sortedCards?.length
+          ? sortedCards
+          : searchedCards;
+
+        sortedCards =
+          filters.manaValueSort === "ASC"
+            ? sortCardsByManaValue(cardsToSort)
+            : filters.manaValueSort === "DESC"
+            ? sortCardsByManaValue(cardsToSort, false)
+            : cardsToSort;
+      }
+    } else {
+      sortedCards = sortCardsByCollectorNumber(searchedCards);
+    }
+
+    setFilteredCards(filterCards(sortedCards, filters));
+  }, [search, filters]);
 
   useEffect(() => {
     if (!cards?.length) return;
@@ -94,9 +152,10 @@ export default function SetPage() {
     <ScrollView>
       <View className="flex-1 flex gap-6 px-11 py-8 bg-background-100 min-h-[100vh]">
         <BoxHeader
+          className="!pb-0"
           title={set.name}
           subtitle={set.cardCount + " Cards | Released " + set.releasedAt}
-          className="!pb-0"
+          end={<FilterBar clear setFilters={setFilters} />}
         />
 
         <SearchBar
@@ -106,7 +165,7 @@ export default function SetPage() {
           placeholder={"Find a " + set.name + " Card"}
         />
 
-        {filteredCards?.length > 0 && <TabBar hideBorder tabs={tabs} />}
+        {filteredCards?.length > 0 && <TabBar hideBorder tabs={tabs}></TabBar>}
 
         {!filteredCards?.length && (
           <View className="flex flex-1 items-center justify-center max-h-fit">
