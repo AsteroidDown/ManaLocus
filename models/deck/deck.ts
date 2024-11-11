afb28274b6a11be4b81ab5b5eb503f89f37c5661
AsteroidDown <jasper@stahmer.ca>
2024-11-10 22:51:49 -0500

    Added user to deck model

--- models/deck/deck.ts
+++ models/deck/deck.ts
@@ -1,8 +1,11 @@
 import { MTGColorSymbol } from "@/constants/mtg/mtg-colors";
 import { MTGFormat } from "@/constants/mtg/mtg-format";
+import { User } from "../user/user";
 
 export interface Deck {
   id: string;
+  userId: string;
+  user?: User;
   name: string;
   featuredArtUrl: string;
   format: MTGFormat;
