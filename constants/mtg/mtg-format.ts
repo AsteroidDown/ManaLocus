f58833853de179500fb1c6781ad5d75e346342eb
AsteroidDown <jasper@stahmer.ca>
2024-11-10 19:23:24 -0500

    Created MTGFormat constants

---
+++ constants/mtg/mtg-format.ts
@@ -0,0 +1,48 @@
+export type MTGFormat =
+  | "standard"
+  | "pioneer"
+  | "modern"
+  | "legacy"
+  | "vintage"
+  | "commander"
+  | "oathbreaker"
+  | "alchemy"
+  | "explorer"
+  | "historic"
+  | "timeless"
+  | "brawl"
+  | "pauper"
+  | "penny"
+  | "future"
+  | "gladiator"
+  | "standardbrawl"
+  | "paupercommander"
+  | "duel"
+  | "oldschool"
+  | "premodern"
+  | "pred";
+
+export enum MTGFormats {
+  STANDARD = "standard",
+  PIONEER = "pioneer",
+  MODERN = "modern",
+  LEGACY = "legacy",
+  VINTAGE = "vintage",
+  COMMANDER = "commander",
+  OATHBREAKER = "oathbreaker",
+  ALCHEMY = "alchemy",
+  EXPLORER = "explorer",
+  HISTORIC = "historic",
+  TIMELESS = "timeless",
+  BRAWL = "brawl",
+  PAUPER = "pauper",
+  PENNY = "penny",
+  FUTURE = "future",
+  GLADIATOR = "gladiator",
+  STANDARDBRAWL = "standardbrawl",
+  PAUPERCOMMANDER = "paupercommander",
+  DUEL = "duel",
+  OLDSCHOOL = "oldschool",
+  PREMODERN = "premodern",
+  PRED = "pred",
+}
