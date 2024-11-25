967f68591f38eb7e1a5320d93b32ded36bb6ffab
AsteroidDown <jasper@stahmer.ca>
2024-11-25 00:12:21 -0500

    Added card grouping in deck card gallery

--- components/decks/deck-card-gallery.tsx
+++ components/decks/deck-card-gallery.tsx
@@ -1,8 +1,11 @@
+import { BoardTypes } from "@/constants/boards";
 import { titleCase } from "@/functions/text-manipulation";
 import { Deck, DeckCard } from "@/models/deck/deck";
+import { Link } from "expo-router";
 import React, { useEffect } from "react";
 import { View } from "react-native";
 import CardText from "../cards/card-text";
+import Divider from "../ui/divider/divider";
 import TabBar from "../ui/tabs/tab-bar";
 import Text from "../ui/text/text";
 
@@ -11,8 +14,14 @@ export interface DeckCardGalleryProps {
 }
 
 export default function DeckCardGallery({ deck }: DeckCardGalleryProps) {
+  const [boardType, setBoardType] = React.useState(BoardTypes.MAIN);
+
   const [boardCards, setBoardCards] = React.useState(
     {} as { [key: string]: DeckCard[] }
+  );
+
+  const [groupedCards, setGroupedCards] = React.useState(
+    [] as { title: string; cards: DeckCard[] }[]
   );
 
   useEffect(() => {
@@ -24,18 +33,45 @@ export default function DeckCardGallery({ deck }: DeckCardGalleryProps) {
       maybe: deck.maybe,
       acquire: deck.acquire,
     });
+
+    setBoardType(BoardTypes.MAIN);
   }, [deck]);
+
+  useEffect(() => {
+    if (!deck) return;
+
+    const groupedCards = boardCards[boardType]?.reduce((acc, card) => {
+      if (!acc[card.cardType]) acc[card.cardType] = [card];
+      else acc[card.cardType].push(card);
+
+      return acc;
+    }, {} as { [key: string]: DeckCard[] });
+
+    if (!groupedCards) return;
+    setGroupedCards(
+      Object.keys(groupedCards)?.map((key) => ({
+        title: key,
+        cards: groupedCards[key],
+      })) ?? []
+    );
+  }, [boardCards, boardType]);
 
   return (
     <View className="flex flex-row flex-wrap gap-4 w-full">
       <TabBar
         hideBorder
-        tabs={["main", "side", "maybe", "acquire"].map((board, boardIndex) => ({
+        tabs={[
+          BoardTypes.MAIN,
+          BoardTypes.SIDE,
+          BoardTypes.MAYBE,
+          BoardTypes.ACQUIRE,
+        ].map((board) => ({
           title: titleCase(board),
+          onClick: () => setBoardType(board),
           children: (
-            <View className="flex">
-              {boardCards[board]?.map((card, index) => (
-                <DeckCardHeader key={index} card={card} />
+            <View className="flex flex-row flex-wrap gap-4 w-full mt-2 columns-4">
+              {groupedCards?.map(({ title, cards }) => (
+                <Column key={title} title={title} cards={cards} />
               ))}
             </View>
           ),
@@ -45,33 +81,38 @@ export default function DeckCardGallery({ deck }: DeckCardGalleryProps) {
   );
 }
 
-function DeckCardHeader({ card }: { card: DeckCard }) {
+function Column({ title, cards }: { title: string; cards?: DeckCard[] }) {
   return (
-    <View className="flex flex-row justify-between items-center px-2 py-1 rounded-full hover:bg-primary-200 transition-all duration-300">
-      <View className="flex flex-row items-center gap-2">
-        <Text>{card.count}</Text>
-        <Text thickness="medium">{card.name}</Text>
-      </View>
+    <View className="flex-1 flex min-w-[256px]">
+      <Text size="lg" thickness="bold">
+        {titleCase(title)}
+      </Text>
 
-      <CardText text={card.manaCost} />
+      <Divider thick className="!border-background-200 my-1" />
+
+      <View className="flex gap-0.5">
+        {cards?.map((card, index) => (
+          <DeckCardHeader key={index} card={card} />
+        ))}
+      </View>
     </View>
   );
 }
 
-function Column({ title, cards }: { title: string; cards?: DeckCard[] }) {
+function DeckCardHeader({ card }: { card: DeckCard }) {
   return (
-    <View className="flex-1 flex min-w-[256px]">
-      <Text size="lg" thickness="bold" className="mb-2">
-        {title}
-      </Text>
-
-      <View className="flex gap-0.5">
-        {cards?.map((card, index) => (
-          <View key={index} className="rounded-xl overflow-hidden">
-            <Text>{card.name}</Text>
-          </View>
-        ))}
+    <Link
+      href={`cards/${card.setId}/${card.collectorNumber}`}
+      className="flex flex-row gap-2 justify-between items-center px-2 py-0.5 rounded-full hover:bg-primary-200 transition-all duration-300"
+    >
+      <View className="flex-1 flex flex-row items-center gap-2">
+        <Text>{card.count}</Text>
+        <Text truncate thickness="medium">
+          {card.name}
+        </Text>
       </View>
-    </View>
+
+      <CardText text={card.manaCost} />
+    </Link>
   );
 }
