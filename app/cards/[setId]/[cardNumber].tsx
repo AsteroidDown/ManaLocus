b5a6d82f7631107c8445cd3fc48c4cfa4302c3d5
AsteroidDown <jasper@stahmer.ca>
2024-12-04 23:43:35 -0500

    Added decks with card to card page

--- app/cards/[setId]/[cardNumber].tsx
+++ app/cards/[setId]/[cardNumber].tsx
@@ -1,6 +1,7 @@
 import CardDetailedPreview from "@/components/cards/card-detailed-preview";
 import { CardLegalities } from "@/components/cards/card-legalities";
 import CardPrintsList from "@/components/cards/card-prints-list";
+import DecksWithCard from "@/components/decks/decks-with-card";
 import Box from "@/components/ui/box/box";
 import ScryfallService from "@/hooks/services/scryfall.service";
 import { Card } from "@/models/card/card";
@@ -25,25 +26,29 @@ export default function SetPage() {
 
   return (
     <ScrollView>
-      <View className="flex items-center bg-background-100 min-h-[100vh] pt-6">
-        <View className="flex flex-row flex-wrap gap-4 max-w-full">
-          <CardDetailedPreview
-            fullHeight
-            hideLegalities
-            card={card}
-            className="max-h-fit !bg-transparent !p-0"
-          />
+      <View className="flex flex-1 gap-4 px-11 py-8 min-h-[100vh] bg-background-100">
+        <View className="flex items-center bg-background-100 pt-6">
+          <View className="flex flex-row flex-wrap gap-4 max-w-full">
+            <CardDetailedPreview
+              fullHeight
+              hideLegalities
+              card={card}
+              className="max-h-fit !bg-transparent !p-0"
+            />
 
-          <View className="flex gap-4">
-            <Box shade={200}>
-              <CardPrintsList card={card} />
-            </Box>
+            <View className="flex gap-4">
+              <Box shade={200}>
+                <CardPrintsList card={card} />
+              </Box>
 
-            <Box shade={200}>
-              <CardLegalities card={card} />
-            </Box>
+              <Box shade={200}>
+                <CardLegalities card={card} />
+              </Box>
+            </View>
           </View>
         </View>
+
+        <DecksWithCard card={card} />
       </View>
     </ScrollView>
   );
