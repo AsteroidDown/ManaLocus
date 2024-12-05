eb3232e92e0b6ecd7b2b10eb6df27a98254428e1
AsteroidDown <jasper@stahmer.ca>
2024-12-04 23:43:16 -0500

    Created DecksWithCard component

--- models/deck/dtos/deck-filters.dto.ts
+++ models/deck/dtos/deck-filters.dto.ts
@@ -3,4 +3,6 @@ import { MTGFormat } from "@/constants/mtg/mtg-format";
 export interface DeckFiltersDTO {
   search?: string | null;
   deckFormat?: MTGFormat | null;
+
+  scryfallIds?: string[] | null;
 }
