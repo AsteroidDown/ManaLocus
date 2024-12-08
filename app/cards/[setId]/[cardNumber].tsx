1361bb74e59099c1f5ebece1d75f415c140412ea
AsteroidDown <jasper@stahmer.ca>
2024-12-08 17:49:11 -0500

    Updated card page layout

--- app/cards/[setId]/[cardNumber].tsx
+++ app/cards/[setId]/[cardNumber].tsx
@@ -3,6 +3,7 @@ import { CardLegalities } from "@/components/cards/card-legalities";
 import CardPrintsList from "@/components/cards/card-prints-list";
 import DecksWithCard from "@/components/decks/decks-with-card";
 import Box from "@/components/ui/box/box";
+import Text from "@/components/ui/text/text";
 import ScryfallService from "@/hooks/services/scryfall.service";
 import { Card } from "@/models/card/card";
 import { useLocalSearchParams } from "expo-router";
@@ -27,19 +28,29 @@ export default function SetPage() {
   return (
     <ScrollView>
       <View className="flex flex-1 gap-4 px-16 py-8 min-h-[100vh] bg-background-100">
-        <View className="flex items-center bg-background-100 pt-6">
-          <View className="flex flex-row flex-wrap gap-4 max-w-full">
-            <CardDetailedPreview
-              fullHeight
-              hideLegalities
-              card={card}
-              className="max-h-fit !bg-transparent !p-0"
-            />
+        <View className="flex flex-row flex-wrap justify-center gap-4 max-w-full pt-6">
+          <CardDetailedPreview
+            fullHeight
+            hideLegalities
+            card={card}
+            className="max-h-fit !bg-transparent !p-0"
+          />
 
-            <View className="flex gap-4">
+          <View className="flex gap-4">
+            <View className="flex flex-col gap-2">
+              <Text size="lg" thickness="bold">
+                Prints
+              </Text>
+
               <Box shade={200}>
                 <CardPrintsList card={card} />
               </Box>
+            </View>
+
+            <View className="flex flex-col gap-2">
+              <Text size="lg" thickness="bold">
+                Legalities
+              </Text>
 
               <Box shade={200}>
                 <CardLegalities card={card} />
