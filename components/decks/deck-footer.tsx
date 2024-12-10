9d63656496b0c1ae36c26cf501c4a5b3ea5c1f41
AsteroidDown <jasper@stahmer.ca>
2024-12-09 22:28:01 -0500

    Added card type symbols and counts to deck footer

--- components/decks/deck-footer.tsx
+++ components/decks/deck-footer.tsx
@@ -1,8 +1,9 @@
+import { groupCardsByType } from "@/functions/card-grouping";
 import { currency, titleCase } from "@/functions/text-manipulation";
 import { Deck } from "@/models/deck/deck";
 import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import React, { useEffect } from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import CardImportExportModal from "../cards/card-import-export-modal";
 import Button from "../ui/button/button";
 import Text from "../ui/text/text";
@@ -14,6 +15,15 @@ export default function DeckFooter({ deck }: { deck: Deck }) {
   const [sideCards, setSideCards] = React.useState(0);
   const [totalValue, setTotalValue] = React.useState(0);
 
+  const [creatureCount, setCreatureCount] = React.useState(0);
+  const [instantCount, setInstantCount] = React.useState(0);
+  const [sorceryCount, setSorceryCount] = React.useState(0);
+  const [artifactCount, setArtifactCount] = React.useState(0);
+  const [enchantmentCount, setEnchantmentCount] = React.useState(0);
+  const [planeswalkerCount, setPlaneswalkerCount] = React.useState(0);
+  const [battleCount, setBattleCount] = React.useState(0);
+  const [landCount, setLandCount] = React.useState(0);
+
   useEffect(() => {
     if (!deck) return;
 
@@ -24,6 +34,37 @@ export default function DeckFooter({ deck }: { deck: Deck }) {
         (acc, card) => acc + (card.prices?.usd ?? 0) * card.count,
         0
       )
+    );
+
+    const groupedCards = groupCardsByType(deck.main);
+
+    setCreatureCount(
+      groupedCards.creature?.reduce((acc, card) => (acc += card.count), 0) || 0
+    );
+    setInstantCount(
+      groupedCards.instant?.reduce((acc, card) => (acc += card.count), 0) || 0
+    );
+    setSorceryCount(
+      groupedCards.sorcery?.reduce((acc, card) => (acc += card.count), 0) || 0
+    );
+    setArtifactCount(
+      groupedCards.artifact?.reduce((acc, card) => (acc += card.count), 0) || 0
+    );
+    setEnchantmentCount(
+      groupedCards.enchantment?.reduce((acc, card) => (acc += card.count), 0) ||
+        0
+    );
+    setPlaneswalkerCount(
+      groupedCards.planeswalker?.reduce(
+        (acc, card) => (acc += card.count),
+        0
+      ) || 0
+    );
+    setBattleCount(
+      groupedCards.battle?.reduce((acc, card) => (acc += card.count), 0) || 0
+    );
+    setLandCount(
+      groupedCards.land?.reduce((acc, card) => (acc += card.count), 0) || 0
     );
   }, [deck]);
 
@@ -44,7 +85,93 @@ export default function DeckFooter({ deck }: { deck: Deck }) {
         <Text>{currency(totalValue)}</Text>
       </View>
 
-      <View className="flex flex-row gap-2">
+      <View className="flex flex-row items-center gap-4">
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/creature.png")}
+          />
+
+          <Text>{creatureCount}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/instant.png")}
+          />
+
+          <Text>{instantCount}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/sorcery.png")}
+          />
+
+          <Text>{sorceryCount}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/artifact.png")}
+          />
+
+          <Text>{artifactCount}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/enchantment.png")}
+          />
+
+          <Text>{enchantmentCount}</Text>
+        </View>
+
+        {planeswalkerCount > 0 && (
+          <View className="flex flex-row items-center gap-2">
+            <Image
+              resizeMode="contain"
+              className="max-h-4 max-w-4"
+              source={require("assets/mtg-types/planeswalker.png")}
+            />
+
+            <Text>{planeswalkerCount}</Text>
+          </View>
+        )}
+
+        {battleCount > 0 && (
+          <View className="flex flex-row items-center gap-2">
+            <Image
+              resizeMode="contain"
+              className="max-h-4 max-w-4"
+              source={require("assets/mtg-types/battle.png")}
+            />
+
+            <Text>{battleCount}</Text>
+          </View>
+        )}
+
+        <View className="flex flex-row items-center gap-2">
+          <Image
+            resizeMode="contain"
+            className="max-h-4 max-w-4"
+            source={require("assets/mtg-types/land.png")}
+          />
+
+          <Text>{landCount}</Text>
+        </View>
+
+        <View className="h-5 border-r rounded-lg border-white -mr-2 ml-1" />
+
         <Button
           rounded
           type="clear"
