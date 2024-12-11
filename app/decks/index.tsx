3439f08791f377b12e906fb2e667283634a39f4e
AsteroidDown <jasper@stahmer.ca>
2024-12-10 21:27:12 -0500

    Created deck gallery

--- app/decks/index.tsx
+++ app/decks/index.tsx
@@ -1,36 +1,13 @@
-import DeckCard from "@/components/decks/deck-card";
+import DeckGallery from "@/components/decks/deck-gallery";
 import BoxHeader from "@/components/ui/box/box-header";
 import Button from "@/components/ui/button/button";
-import Divider from "@/components/ui/divider/divider";
-import Input from "@/components/ui/input/input";
-import Select from "@/components/ui/input/select";
-import Text from "@/components/ui/text/text";
-import { MTGFormats } from "@/constants/mtg/mtg-format";
 import UserContext from "@/contexts/user/user.context";
-import { titleCase } from "@/functions/text-manipulation";
 import DeckService from "@/hooks/services/deck.service";
-import { Deck } from "@/models/deck/deck";
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "expo-router";
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { ScrollView, View } from "react-native";
 
 export default function DecksPage() {
   const { user } = useContext(UserContext);
-
-  const [decks, setDecks] = React.useState([] as Deck[]);
-
-  const [filtersOpen, setFiltersOpen] = React.useState(false);
-
-  const [search, setSearch] = React.useState("");
-  const [format, setFormat] = React.useState(null as MTGFormats | null);
-
-  useEffect(() => {
-    DeckService.getMany({
-      ...(search && { search }),
-      ...(format && { deckFormat: format }),
-    }).then((decks) => setDecks(decks));
-  }, [user, format, search]);
 
   function createDeck() {
     if (!user) return;
@@ -47,81 +24,7 @@ export default function DecksPage() {
           end={user && <Button text="Create Deck" onClick={createDeck} />}
         />
 
-        <View className="flex flex-row gap-4">
-          <Input
-            label="Search"
-            placeholder="Search for a deck or card"
-            value={search}
-            onChange={setSearch}
-          />
-
-          <Button
-            icon={faFilter}
-            className="self-end"
-            type={filtersOpen ? "default" : "outlined"}
-            onClick={() => setFiltersOpen(!filtersOpen)}
-          />
-        </View>
-
-        <View
-          className={`${
-            filtersOpen ? "max-h-[1000px]" : "max-h-0"
-          } flex gap-4 overflow-hidden transition-all duration-300`}
-        >
-          <View className="flex gap-2 z-[11]">
-            <Text size="lg" thickness="bold">
-              Filters
-            </Text>
-
-            <Divider thick className="!border-background-200" />
-
-            <Select
-              label="Format"
-              value={format}
-              onChange={(change) => setFormat(change)}
-              options={[
-                { label: "All", value: null },
-                ...Object.keys(MTGFormats).map((key) => {
-                  return {
-                    label: titleCase(key),
-                    value: (MTGFormats as any)[key],
-                  };
-                }),
-              ]}
-            />
-          </View>
-
-          {/* <View className="flex gap-2 z-10">
-            <Text size="lg" thickness="bold">
-              Sort Options
-            </Text>
-
-            <Divider thick className="!border-background-200" />
-
-            <Select
-              label="Format"
-              value={format}
-              onChange={(change) => setFormat(change)}
-              options={[
-                { label: "All", value: null },
-                ...Object.keys(MTGFormats).map((key) => {
-                  return {
-                    label: titleCase(key),
-                    value: (MTGFormats as any)[key],
-                  };
-                }),
-              ]}
-            />
-          </View> */}
-        </View>
-
-        <View className="flex flex-row flex-wrap gap-4">
-          {decks?.map((deck, index) => (
-            <Link key={deck.id + index} href={`decks/${deck.id}`}>
-              <DeckCard deck={deck} />
-            </Link>
-          ))}
-        </View>
+        <DeckGallery />
       </View>
     </ScrollView>
   );
---
+++ components/decks/deck-gallery.tsx
@@ -0,0 +1,113 @@
+import Button from "@/components/ui/button/button";
+import Input from "@/components/ui/input/input";
+import Select from "@/components/ui/input/select";
+import { MTGFormats } from "@/constants/mtg/mtg-format";
+import UserContext from "@/contexts/user/user.context";
+import { titleCase } from "@/functions/text-manipulation";
+import DeckService from "@/hooks/services/deck.service";
+import { Deck } from "@/models/deck/deck";
+import {
+  DeckSortType,
+  DeckSortTypes,
+} from "@/models/deck/dtos/deck-filters.dto";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "expo-router";
+import React, { useContext, useEffect } from "react";
+import { View } from "react-native";
+import DeckCard from "./deck-card";
+
+export default function DeckGallery() {
+  const { user } = useContext(UserContext);
+
+  const [decks, setDecks] = React.useState([] as Deck[]);
+
+  const [filtersOpen, setFiltersOpen] = React.useState(false);
+  const [overflow, setOverflow] = React.useState(false);
+
+  const [search, setSearch] = React.useState("");
+  const [format, setFormat] = React.useState(null as MTGFormats | null);
+  const [sort, setSort] = React.useState(DeckSortTypes.CREATED as DeckSortType);
+
+  useEffect(() => {
+    DeckService.getMany({
+      ...(search && { search }),
+      ...(format && { deckFormat: format }),
+      ...(sort && { sort }),
+    }).then((decks) => setDecks(decks));
+  }, [user, format, search, sort]);
+
+  useEffect(() => {
+    if (filtersOpen) setTimeout(() => setOverflow(filtersOpen), 300);
+    else setOverflow(false);
+  }, [filtersOpen]);
+
+  return (
+    <View className="flex gap-4">
+      <View className="flex flex-row gap-4">
+        <Input
+          label="Search"
+          placeholder="Search for a deck or card"
+          value={search}
+          onChange={setSearch}
+        />
+
+        <Button
+          icon={faFilter}
+          className="self-end"
+          type={filtersOpen ? "default" : "outlined"}
+          onClick={() => setFiltersOpen(!filtersOpen)}
+        />
+      </View>
+
+      <View
+        className={`${filtersOpen ? "max-h-[1000px]" : "max-h-0"} ${
+          !overflow && "overflow-hidden"
+        } flex gap-4 z-[11] transition-all duration-300`}
+      >
+        <View className="flex flex-row gap-4">
+          <Select
+            label="Format"
+            value={format}
+            className="!flex-[2]"
+            onChange={(change) => setFormat(change)}
+            options={[
+              { label: "All", value: null },
+              ...Object.keys(MTGFormats).map((key) => {
+                return {
+                  label: titleCase(key),
+                  value: (MTGFormats as any)[key],
+                };
+              }),
+            ]}
+          />
+
+          <Select
+            label="Sort"
+            value={sort}
+            onChange={(change) => setSort(change)}
+            options={[
+              { label: "Created", value: DeckSortTypes.CREATED },
+              {
+                label: "Created (Old to New)",
+                value: DeckSortTypes.CREATED_REVERSE,
+              },
+              { label: "Updated", value: DeckSortTypes.UPDATED },
+              {
+                label: "Updated (Old to New)",
+                value: DeckSortTypes.UPDATED_REVERSE,
+              },
+            ]}
+          />
+        </View>
+      </View>
+
+      <View className="flex flex-row flex-wrap gap-4 z-[10]">
+        {decks?.map((deck, index) => (
+          <Link key={deck.id + deck.name + index} href={`decks/${deck.id}`}>
+            <DeckCard deck={deck} />
+          </Link>
+        ))}
+      </View>
+    </View>
+  );
+}
