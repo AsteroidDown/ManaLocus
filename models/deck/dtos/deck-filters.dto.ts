6f2cf8e34fb9a1fd39c4172d60af531e2ff2de6a
AsteroidDown <jasper@stahmer.ca>
2024-12-10 21:26:52 -0500

    Created deck sort types

--- models/deck/dtos/deck-filters.dto.ts
+++ models/deck/dtos/deck-filters.dto.ts
@@ -3,6 +3,15 @@ import { MTGFormat } from "@/constants/mtg/mtg-format";
 export interface DeckFiltersDTO {
   search?: string | null;
   deckFormat?: MTGFormat | null;
-
   cardNames?: string[] | null;
+  sort?: DeckSortType | null;
+}
+
+export type DeckSortType = "created" | "-created" | "updated" | "-updated";
+
+export enum DeckSortTypes {
+  CREATED = "created",
+  CREATED_REVERSE = "-created",
+  UPDATED = "updated",
+  UPDATED_REVERSE = "-updated",
 }
