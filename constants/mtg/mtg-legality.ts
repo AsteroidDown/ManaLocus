86a95537110f9d8d399e35f2f5d9f4af693cbc07
AsteroidDown <jasper@stahmer.ca>
2024-12-12 23:34:15 -0500

    Added legality enum

--- constants/mtg/mtg-legality.ts
+++ constants/mtg/mtg-legality.ts
@@ -1,1 +1,8 @@
-export type MTGLegality = "legal" | "not_legal" | "restricted";
+export type MTGLegality = "legal" | "not_legal" | "restricted" | "banned";
+
+export enum MTGLegalities {
+  LEGAL = "legal",
+  NOT_LEGAL = "not_legal",
+  RESTRICTED = "restricted",
+  BANNED = "banned",
+}
