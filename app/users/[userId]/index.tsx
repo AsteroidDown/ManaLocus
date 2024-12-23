79c919a2a0a7db12ec503ff4dac2b09b8cb7d8ba
AsteroidDown <jasper@stahmer.ca>
2024-12-23 13:55:58 -0500

    Added tabs on user page for favorites and settings

--- app/users/[userId]/index.tsx
+++ app/users/[userId]/index.tsx
@@ -1,44 +1,18 @@
 import DeckGallery from "@/components/decks/deck-gallery";
-import BoxHeader from "@/components/ui/box/box-header";
-import Button from "@/components/ui/button/button";
-import Text from "@/components/ui/text/text";
 import UserContext from "@/contexts/user/user.context";
-import UserService from "@/hooks/services/user.service";
-import { router } from "expo-router";
 import React, { useContext } from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
-  const { user, setUser } = useContext(UserContext);
-
-  function logout() {
-    UserService.logout().then(() => {
-      setUser(null);
-      router.push("../..");
-    });
-  }
+  const { user } = useContext(UserContext);
 
   if (!user) return null;
 
   return (
-    <SafeAreaView className="flex-1 flex w-full h-full bg-dark-100">
-      <ScrollView>
-        <View className="flex-1 flex gap-4 w-full min-h-[100vh] px-16 py-8 bg-background-100">
-          <View className="flex flex-row justify-between">
-            <Text size="2xl" thickness="medium">
-              {user?.name}
-            </Text>
-
-            <Button text="Logout" action="danger" onClick={() => logout()} />
-          </View>
-
-          <BoxHeader title="Your Decks" className="!pb-0" />
-
-          <DeckGallery userId={user.id} />
-        </View>
-
-        <DeckGallery />
+    <SafeAreaView className="flex-1 flex w-full h-full bg-background-100">
+      <ScrollView className="my-4">
+        <DeckGallery userId={user.id} />
       </ScrollView>
     </SafeAreaView>
   );
--- app/users/_layout.tsx
+++ app/users/_layout.tsx
@@ -1,14 +1,57 @@
+import Button from "@/components/ui/button/button";
+import { TabProps } from "@/components/ui/tabs/tab";
+import TabBar from "@/components/ui/tabs/tab-bar";
+import Text from "@/components/ui/text/text";
+import UserContext from "@/contexts/user/user.context";
 import "@/global.css";
-import { Stack } from "expo-router";
-import React from "react";
-import { SafeAreaView } from "react-native";
+import UserService from "@/hooks/services/user.service";
+import { router } from "expo-router";
+import React, { useContext } from "react";
+import { SafeAreaView, View } from "react-native";
 
-export default function RootLayout() {
+export default function UserLayout() {
+  const { user, setUser } = useContext(UserContext);
+
+  if (!user) return null;
+
+  const tabs: TabProps[] = [
+    {
+      title: "Your Decks",
+      link: `users/${user.id}`,
+      name: "decks",
+    },
+    {
+      title: "Favorites",
+      link: `users/${user.id}/favorites`,
+      name: "favorites",
+    },
+    {
+      title: "Settings",
+      link: `users/${user.id}/settings`,
+      name: "settings",
+    },
+  ];
+
+  function logout() {
+    UserService.logout().then(() => {
+      setUser(null);
+      router.push("../..");
+    });
+  }
+
   return (
     <SafeAreaView className="flex w-full h-full bg-background-100">
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="[userId]/index" />
-      </Stack>
+      <View className="flex-1 flex gap-4 w-full min-h-[100vh] px-16 py-8 bg-background-100">
+        <View className="flex flex-row justify-between">
+          <Text size="2xl" thickness="medium">
+            {user?.name}
+          </Text>
+
+          <Button text="Logout" action="danger" onClick={() => logout()} />
+        </View>
+
+        <TabBar hideBorder tabs={tabs} />
+      </View>
     </SafeAreaView>
   );
 }
