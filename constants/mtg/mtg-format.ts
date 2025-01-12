c580a71d8100c64f2824fe1e9c68a793f6e9291a
AsteroidDown <jasper@stahmer.ca>
2025-01-12 13:06:17 -0500

    Added format restrictions map

--- constants/mtg/mtg-format.ts
+++ constants/mtg/mtg-format.ts
@@ -1,3 +1,5 @@
+import { MTGRarities, MTGRarity } from "./mtg-rarity";
+
 export type MTGFormat =
   | "standard"
   | "pioneer"
@@ -48,3 +50,199 @@ export enum MTGFormats {
   PREMODERN = "premodern",
   PRED = "pred",
 }
+
+export interface MTGFormatRestrictions {
+  deckMinSize?: number;
+  deckMaxSize?: number;
+  uniqueCardCount?: number;
+  sideBoard?: boolean;
+  commander?: boolean;
+  signatureSpell?: boolean;
+  maxRarity?: MTGRarity;
+  maxTix?: number;
+}
+
+export const MTGFormatRestrictionsMap = new Map<
+  MTGFormat,
+  MTGFormatRestrictions
+>([
+  [
+    MTGFormats.STANDARD,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.PIONEER,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.MODERN,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.LEGACY,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.VINTAGE,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.COMMANDER,
+    {
+      deckMinSize: 100,
+      deckMaxSize: 100,
+      uniqueCardCount: 1,
+      commander: true,
+    },
+  ],
+  [
+    MTGFormats.OATHBREAKER,
+    {
+      deckMinSize: 60,
+      deckMaxSize: 60,
+      uniqueCardCount: 1,
+      commander: true,
+      signatureSpell: true,
+    },
+  ],
+  [
+    MTGFormats.ALCHEMY,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.EXPLORER,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.HISTORIC,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.TIMELESS,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.BRAWL,
+    {
+      deckMinSize: 60,
+      deckMaxSize: 60,
+      uniqueCardCount: 1,
+      commander: true,
+    },
+  ],
+  [
+    MTGFormats.PAUPER,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+      maxRarity: MTGRarities.COMMON,
+    },
+  ],
+  [
+    MTGFormats.PENNY,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+      maxTix: 0.02,
+    },
+  ],
+  [
+    MTGFormats.GLADIATOR,
+    {
+      deckMinSize: 100,
+      deckMaxSize: 100,
+      uniqueCardCount: 1,
+    },
+  ],
+  [
+    MTGFormats.STANDARDBRAWL,
+    {
+      deckMinSize: 60,
+      deckMaxSize: 60,
+      uniqueCardCount: 1,
+      commander: true,
+    },
+  ],
+  [
+    MTGFormats.PAUPERCOMMANDER,
+    {
+      deckMinSize: 100,
+      deckMaxSize: 100,
+      uniqueCardCount: 1,
+      commander: true,
+      maxRarity: MTGRarities.COMMON,
+    },
+  ],
+  [
+    MTGFormats.DUEL,
+    {
+      deckMinSize: 100,
+      deckMaxSize: 100,
+      uniqueCardCount: 1,
+      commander: true,
+    },
+  ],
+  [
+    MTGFormats.OLDSCHOOL,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.PREMODERN,
+    {
+      deckMinSize: 60,
+      uniqueCardCount: 4,
+      sideBoard: true,
+    },
+  ],
+  [
+    MTGFormats.PRED,
+    {
+      deckMinSize: 100,
+      deckMaxSize: 100,
+      uniqueCardCount: 1,
+      commander: true,
+    },
+  ],
+]);
