e8ce35a1d9b457d1a45dda339713f3a73b2b1da2
AsteroidDown <jasper@stahmer.ca>
2025-01-27 22:17:44 -0500

    Only fade out

--- components/ui/navigation/loading.tsx
+++ components/ui/navigation/loading.tsx
@@ -27,9 +27,9 @@ export default function LoadingView() {
   if (isLoading || fade) {
     return (
       <View
-        className={`absolute top-0 left-0 flex-1 flex justify-center items-center w-full h-[100dvh] bg-background-100 transition-all duration-500 ${
-          fade ? "opacity-0" : "opacity-100"
-        }`}
+        className={`absolute top-0 left-0 flex-1 flex justify-center items-center w-full h-[100dvh] bg-background-100 ${
+          isLoading && "transition-all duration-500"
+        } ${fade ? "opacity-0" : "opacity-100"}`}
       >
         <Image
           source={require("assets/Logo.png")}
