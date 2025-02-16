4012ee5dfe349f96314bbae8ed6d459ed19c5aec
AsteroidDown <jasper@stahmer.ca>
2025-02-16 15:22:25 -0500

    Updated currency text manipulation for negatives

--- functions/text-manipulation.ts
+++ functions/text-manipulation.ts
@@ -11,12 +11,15 @@ export function currency(value?: number | null, euro = false) {
   const symbol = euro ? "€" : "$";
   if (!value) return symbol + "0.00";
 
+  const negative = value < 0;
+  if (negative) value *= -1;
+
   if (typeof value !== "number") {
     const valueNumber = Number(value);
     if (isNaN(valueNumber)) return symbol + "0.00";
 
-    return symbol + valueNumber.toFixed(2);
+    return (negative ? "-" : "") + symbol + valueNumber.toFixed(2);
   }
 
-  return symbol + value.toFixed(2);
+  return (negative ? "-" : "") + symbol + value.toFixed(2);
 }
