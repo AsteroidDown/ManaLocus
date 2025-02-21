9fa55c8afd586b46ff4512795b263140f58bbb30
AsteroidDown <jasper@stahmer.ca>
2025-02-21 00:31:30 -0500

    Updated checkbox disabled style

--- components/ui/checkbox/checkbox.tsx
+++ components/ui/checkbox/checkbox.tsx
@@ -36,7 +36,9 @@ export default function Checkbox({
     >
       <View
         className={`${hovered ? "bg-primary-300 border-primary-200" : ""} ${
-          disabled ? "!border-background-100" : "border-background-200"
+          disabled
+            ? "!border-dark-200 !bg-background-200"
+            : "border-background-200"
         } flex justify-center items-center w-6 h-6 rounded-lg bg-opacity-35 border-2 overflow-hidden transition-all duration-150`}
       >
         <View
@@ -44,18 +46,29 @@ export default function Checkbox({
             checked ? "w-6 h-6" : "w-0 h-0"
           } relative flex justify-center items-center bg-primary-300 rounded-full overflow-hidden transition-all duration-150`}
         >
-          <FontAwesomeIcon icon={faCheck} className="text-white" />
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={disabled ? "text-dark-300" : "text-white"}
+          />
         </View>
       </View>
 
       <View className="flex">
         {label && (
-          <Text size={size ?? "md"} thickness="medium">
+          <Text
+            size={size ?? "md"}
+            thickness="medium"
+            className={disabled ? "text-dark-300" : "text-white"}
+          >
             {label}
           </Text>
         )}
 
-        {text && <Text size="xs">{text}</Text>}
+        {text && (
+          <Text size="xs" className={disabled ? "text-dark-300" : "text-white"}>
+            {text}
+          </Text>
+        )}
       </View>
     </Pressable>
   );
