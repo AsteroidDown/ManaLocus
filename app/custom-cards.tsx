40399eebfd0314524d2134f200e8b8d3049c6abb
AsteroidDown <jasper@stahmer.ca>
2025-04-01 10:24:56 -0400

    Added custom cards

---
+++ app/custom-cards.tsx
@@ -0,0 +1,26 @@
+import Button from "@/components/ui/button/button";
+import Footer from "@/components/ui/navigation/footer";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import { Linking, SafeAreaView, View } from "react-native";
+
+export default function VerifyPage() {
+  useEffect(() => {
+    setTimeout(
+      () => Linking.openURL("https://www.youtube.com/watch?v=2qBlE2-WL60"),
+      1500
+    );
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View className="flex flex-row justify-center flex-1 gap-4 lg:px-16 px-4 py-4 min-h-[100dvh] bg-background-100">
+        <View className="flex flex-col justify-center -mt-10">
+          <Button disabled type="clear" icon={faRotate} text="loading..." />
+        </View>
+      </View>
+
+      <Footer />
+    </SafeAreaView>
+  );
+}
