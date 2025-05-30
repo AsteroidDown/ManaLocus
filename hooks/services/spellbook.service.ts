782ab94eedd9f178ea2eff38162deb70ce0ca6a4
AsteroidDown <jasper@stahmer.ca>
2025-05-30 03:09:50 -0400

    Updated spellbook to backend

--- hooks/services/spellbook.service.ts
+++ hooks/services/spellbook.service.ts
@@ -1,9 +1,9 @@
 import { Deck } from "@/models/deck/deck";
 import { SpellbookList } from "@/models/spellbook/spellbook-list";
-import SpellbookAPI from "../api-methods/spellbook-api-methods";
+import API from "../api-methods/api-methods";
 
 async function getCombos(deck: Deck, limit?: number) {
-  const response: SpellbookList = await SpellbookAPI.post(`find-my-combos`, {
+  const response: SpellbookList = await API.post(`commander-spellbook/`, {
     ...(limit && { limit }),
     main: deck.main.map((card) => ({ card: card.name, quantity: card.count })),
     commanders: [
