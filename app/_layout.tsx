c0d332511cb79870b7663355908fffcd2ea1490d
AsteroidDown <jasper@stahmer.ca>
2025-06-02 00:25:45 -0400

    Updated user setting/fetching

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ import { getLocalStorageJwt } from "@/functions/local-storage/auth-token-local-s
 import {
   getLocalStorageUser,
   removeLocalStorageUser,
+  updateLocalStorageUser,
 } from "@/functions/local-storage/user-local-storage";
 import { getLocalStorageUserPreferences } from "@/functions/local-storage/user-preferences-local-storage";
 import "@/global.css";
@@ -35,27 +36,39 @@ export default function RootLayout() {
   const access = getLocalStorageJwt()?.access;
 
   useEffect(() => {
-    if (!user && access) {
-      const savedUser = getLocalStorageUser();
+    if (user) return;
 
-      if (!savedUser) {
-        removeLocalStorageUser();
-        return;
-      } else if (
-        moment(savedUser.lastLogin).isAfter(moment().subtract(3, "days"))
-      ) {
-        setUser(savedUser);
-        UserService.refresh();
-      } else {
-        UserService.login(savedUser.name, savedUser.password).then((user) => {
-          if (!user) removeLocalStorageUser();
-          else setUser(user);
-        });
-      }
-    } else if (!access) {
+    const savedUser = getLocalStorageUser();
+
+    if (!savedUser || !access) {
       removeLocalStorageUser();
+      return;
     }
 
+    if (moment(savedUser.lastLogin).isAfter(moment().subtract(3, "days"))) {
+      setUser(savedUser);
+
+      UserService.getCurrentUser().then((currentUser) => {
+        if (currentUser) {
+          updateLocalStorageUser(currentUser);
+          setUser(getLocalStorageUser());
+        } else {
+          removeLocalStorageUser();
+        }
+      });
+    } else {
+      UserService.login(savedUser.name, savedUser.password).then((user) => {
+        if (!user) {
+          removeLocalStorageUser();
+        } else {
+          setUser(user);
+          updateLocalStorageUser(user, savedUser.password);
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
     const preferences = getLocalStorageUserPreferences();
     setPreferences(preferences);
 
@@ -65,7 +78,7 @@ export default function RootLayout() {
         PreferenceColorMap[preferences?.color ?? PreferenceColor.DEFAULT][hue]
       );
     });
-  }, [user]);
+  }, []);
 
   return (
     <Host>
