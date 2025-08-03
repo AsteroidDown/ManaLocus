9d988bd8e869839b4eca9ebf50552c86d36f5eab
AsteroidDown <jasper@stahmer.ca>
2025-08-03 10:30:53 -0400

    Added show/hide to secured input

--- components/ui/input/input.tsx
+++ components/ui/input/input.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { TextInput, View, ViewProps } from "react-native";
+import Button from "../button/button";
 import Text from "../text/text";
 
 export interface InputProps extends ViewProps {
@@ -17,7 +19,7 @@ export interface InputProps extends ViewProps {
   squareRight?: boolean;
 
   value?: string;
-  onChange: React.Dispatch<React.SetStateAction<string>>;
+  onChange: Dispatch<SetStateAction<string>>;
   enterAction?: () => void;
 }
 
@@ -41,10 +43,12 @@ export default function Input({
 
   className,
 }: InputProps) {
-  const [text, setText] = React.useState(value ?? "");
+  const [text, setText] = useState(value ?? "");
 
-  const [hovered, setHovered] = React.useState(false);
-  const [focused, setFocused] = React.useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [focused, setFocused] = useState(false);
+
+  const [showText, setShowText] = useState(false);
 
   useEffect(() => {
     if (value !== text) setText(String(value ?? ""));
@@ -63,13 +67,7 @@ export default function Input({
     >
       {label && <Text weight="medium">{label}</Text>}
 
-      <TextInput
-        value={text}
-        multiline={multiline}
-        placeholder={placeholder}
-        tabIndex={disabled ? -1 : 0}
-        secureTextEntry={secured}
-        placeholderTextColor="#8b8b8b"
+      <View
         className={`${
           error
             ? "border-danger-100"
@@ -82,23 +80,43 @@ export default function Input({
           multiline ? "min-h-24 !max-h-[80px]" : "h-8"
         } ${squareLeft ? "rounded-l-none" : ""} ${
           squareRight ? "rounded-r-none" : ""
-        } flex-1 px-3 py-2 max-h-8 min-h-8 color-white rounded-lg text-sm border-2  outline-none transition-all`}
-        onChangeText={setText}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
-        onKeyPress={(e) => {
-          if (e.nativeEvent.key === "Enter") {
-            enterAction?.();
-          }
-        }}
-      />
+        } flex-1 flex flex-row items-center px-3 py-2 max-h-8 min-h-8 color-white rounded-lg text-sm border-2 transition-all`}
+      >
+        <TextInput
+          value={text}
+          multiline={multiline}
+          placeholder={placeholder}
+          tabIndex={disabled ? -1 : 0}
+          secureTextEntry={secured && !showText}
+          className="flex-1 outline-none "
+          placeholderTextColor="#8b8b8b"
+          onChangeText={setText}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          onKeyPress={(e) => {
+            if (e.nativeEvent.key === "Enter") {
+              enterAction?.();
+            }
+          }}
+        />
+
+        {secured && (
+          <Button
+            size="sm"
+            type="clear"
+            className="-mr-3"
+            icon={showText ? faEyeSlash : faEye}
+            onClick={() => setShowText(!showText)}
+          />
+        )}
+      </View>
 
       <View
         className={`${
           error && errorMessage ? "max-h-fit" : "max-h-0 -mt-2"
         } overflow-hidden transition-all duration-300`}
       >
-        <Text size="sm" action="danger">
+        <Text size="md" action="danger">
           {errorMessage}
         </Text>
       </View>
